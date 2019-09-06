bba257b4905a62d6a88ca64e80c7a72b716a6840
Medasele273 <medasele273@gmail.com>
2019-09-06 14:48:08 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+        Garage garage = new Garage();
+        //Vehicle car = new Car();
+       // Vehicle car1 =new Car("AAA111");
+        Car car2= new Car();
+        System.out.println(car2);
+      //  System.out.println(car1);
+    }
+
+}
